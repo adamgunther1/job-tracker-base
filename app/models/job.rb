ec493cbe95286e5d8da5f00301857d49a3fca7ef
adamgunther1 <adamgunther1@gmail.com>
2017-06-06 11:14:03 -0600

    adds tag count functionality

--- app/models/job.rb
+++ app/models/job.rb
@@ -4,5 +4,4 @@ class Job < ActiveRecord::Base
   has_many :jobs_tags
   has_many :tags, through: :jobs_tags
 
-
 end
--- app/models/jobs_tag.rb
+++ app/models/jobs_tag.rb
@@ -2,8 +2,8 @@ class JobsTag < ActiveRecord::Base
   belongs_to :job
   belongs_to :tag 
 
-
   def tag_count(tag)
     JobsTag.where(tag_id: tag.id).count
   end
+
 end
